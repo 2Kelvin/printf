00ea270154f9e6d4e593280df3b970b7fb0b7dd5
BunnyeNyash <otachibosibori@gmail.com>
2023-04-27 06:53:03 -0700

    Add writechar.c, puts.c, and main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _writechar(char c);
+int _puts(char *str);
+int printInt(int num);
+int printBinary(unsigned int num);
+int _printf(const char *format, ...);
+
+#endif
